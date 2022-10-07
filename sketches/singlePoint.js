abd2fa8d803220c355c32f5b4489be1579af9e12
Hunter <h.dyar@chatham.edu>
2022-10-07 17:11:25 -0400

    Work on first draft

--- sketches/singlePoint.js
+++ sketches/singlePoint.js
@@ -1,23 +1,19 @@
-// zdog-demo.js
-function drawSingleFace(elementLeft,elementRight) {
 
-
-    let isSpinning = true;
-    let TAU = Zdog.TAU;//Tau is a full turn.
-
+function drawSinglePoint(canvasElementLeft,canvasElementRight) {
+    let spinning = true;
     let length = 4.25;
     let offset = 1.25;
 
     let threeDAxis = new Zdog.Illustration({
         // set canvas with selector
-        element: elementLeft,
+        element: canvasElementLeft,
         zoom: 20,
         dragRotate: true,
         onDragStart: function () {
-            isSpinning = false;
+            spinning = false;
         },
     });
-
+    console.log(threeDAxis);
     let xLine = new Zdog.Shape({
         addTo: threeDAxis,
         path: [{x: offset, y: 0}, {x: length, y: 0}],
@@ -31,7 +27,7 @@ function drawSingleFace(elementLeft,elementRight) {
         stroke: false,
         color: '#633',
         backface: '#C44',
-        rotate: {y: TAU * 3 / 4},//3/4 turn
+        rotate: {y: Zdog.TAU * 3 / 4},//3/4 turn
         translate: {x: 5}
     });
 
@@ -48,7 +44,7 @@ function drawSingleFace(elementLeft,elementRight) {
         stroke: false,
         color: '#363',
         backface: '#4C4',
-        rotate: {x: TAU / 4},
+        rotate: {x: Zdog.TAU / 4},
         translate: {y: -5}
     });
 
@@ -75,43 +71,29 @@ function drawSingleFace(elementLeft,elementRight) {
         diameter: 2,
     });
 
-    let face3 = new Zdog.Rect({
-        addTo: threeDAxis,
-        width: 3,
-        height: 2,
-        stroke: 0.5,
-        color: '#E62',
-        translate: {x: 3, y: -3, z: 3},
-        rotate: {y: TAU / 5, x: TAU / 3, Z: TAU / 3}
-    });
-
     function animate() {
         // rotate illo each frame
-        if (isSpinning) {
+        if (spinning) {
             threeDAxis.rotate.y += 0.01;
         }
         threeDAxis.updateRenderGraph();
         requestAnimationFrame(animate);
     }
 
-// start animation
+    threeDAxis.updateRenderGraph();
     animate();
 
-// update & render
-    threeDAxis.updateRenderGraph();
+    /////
 
-
-////// 2D axis
     let twoDAxis = new Zdog.Illustration({
         // set canvas with selector
-        element: elementRight,
+        element: canvasElementRight,
         zoom: 20,
         dragRotate: true,
         onDragStart: function () {
             isSpinning = false;
         },
     });
-
 
     let uLine = new Zdog.Shape({
         addTo: twoDAxis,
@@ -126,7 +108,7 @@ function drawSingleFace(elementLeft,elementRight) {
         stroke: false,
         color: '#663',
         backface: '#CC4',
-        rotate: {y: -TAU / 4},
+        rotate: {y: -Zdog.TAU / 4},
         translate: {x: 5}
     });
 
@@ -143,7 +125,7 @@ function drawSingleFace(elementLeft,elementRight) {
         stroke: false,
         color: '#366',
         backface: '#4CC',
-        rotate: {x: TAU / 4},
+        rotate: {x: Zdog.TAU / 4},
         translate: {y: -5}
     });
 
@@ -153,10 +135,18 @@ function drawSingleFace(elementLeft,elementRight) {
         diameter: 2,
     });
 
-    let face2 = new Zdog.Rect({
+
+    //Point!
+    let point3 = new Zdog.Shape({
+        addTo: threeDAxis,
+        stroke: 0.5,
+        color: '#E62',
+        translate: {x: 1, y: -2, z: 3},
+    });
+
+
+    let point2 = new Zdog.Shape({
         addTo: twoDAxis,
-        width: 3,
-        height: 2,
         stroke: 0.5,
         color: '#E62',
         translate: {x: 3, y: -3},
@@ -164,4 +154,6 @@ function drawSingleFace(elementLeft,elementRight) {
 
     twoDAxis.updateRenderGraph();
 }
-drawSingleFace('.zdog-singleFace','.zdog-singleFace');
+
+drawSinglePoint('.zdog-singlePoint3d','.zdog-singlePoint2d2');
+drawSinglePoint('.zdog-singlePoint','.zdog-singlePoint2');
