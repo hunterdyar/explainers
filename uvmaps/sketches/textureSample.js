0058684ca67f818caa73139aa224b903cab9cd51
hdyar <bloop@hdyar.com>
2022-10-08 21:04:31 -0400

    Work on canvas vs. svg for images in UV sampling section. also further reading.

--- uvmaps/sketches/textureSample.js
+++ uvmaps/sketches/textureSample.js
@@ -1,12 +1,59 @@
-let uvCanvas = 'textureSampleCanvas';
-const canvas = document.getElementById(uvCanvas);
+let uvCanvas = 'textureSampleCanvas1';
+const canvas = document.getElementById('textureSampleCanvas3');
 const ctx = canvas.getContext("2d");
 const image = document.getElementById("crateImg");
+let arrows;
+let size = 300;
+
+// get svg element
+let svg = document.querySelector('svg');
+// rendering sizes
+const zoom = 1;
+let sceneWidth = 20;
+let sceneHeight = 20;
+let viewWidth = sceneWidth * zoom;
+let viewHeight = sceneHeight * zoom;
+let svgWidth = svg.getAttribute('width');
+let svgHeight = svg.getAttribute('height');
+// set viewBox using zoom
+svg.setAttribute( 'viewBox', `${-viewWidth/2}  ${-viewHeight/2} ` +
+    `${viewWidth} ${viewHeight}` );
+
 
 image.addEventListener("load", (e) => {
-    //ctx.drawImage(image, 0, 0, 300, 300);
-    drawLabeledUV(uvCanvas);
+
+    arrows = drawLabeledUV('textureSampleCanvas1');
+    renderBoth(ctx,arrows);
+
+    // drawLabeledUV('textureSampleCanvas2');
+    let sampled = drawLabeledUV('textureSampleCanvas3');
+    let point2 = new Zdog.Shape({
+        addTo: sampled,
+        stroke: 0.5,
+        color: '#908373',
+        translate: {x: 5, y: -5},
+    });
+    sampled.updateRenderGraph();
 });
+
+function renderBoth(ctx,arrows) {
+    let anchor = new Zdog.Anchor();
+    //
+    arrows.children.forEach((child) => {
+        anchor.children.push(child);
+    });
+
+    let point2 = new Zdog.Shape({
+        addTo: anchor,
+        stroke: 0.5,
+        color: '#E62',
+        translate: {x: 5, y: -5},
+    });
+
+    anchor.renderGraphCanvas( ctx );
+    anchor.renderGraphSvg( svg );
+    ctx.restore();
+}
 
 //todo: replace this with drawing without Illustration, so the bg doesnt get cleared, so the image can get drawn below it.
 //I think drawing to an SVG will be the way to go.
@@ -61,5 +108,6 @@ function drawLabeledUV(canvas) {
         translate: {y: -9.5, x: -2.6},
     });
     twoDAxis.updateRenderGraph();
+    return twoDAxis;
 }
 
