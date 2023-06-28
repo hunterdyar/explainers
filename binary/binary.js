50114129366dedcac73e74de4c7d4e6a47f7bae5
hunterdyar <bloop@hdyar.com>
2023-06-28 15:53:45 -0400

    alt text; other updates and fixes

--- binary/binary.js
+++ binary/binary.js
@@ -1,11 +1,11 @@
-https://blog.stevenlevithan.com/archives/javascript-roman-numeral-converter
+//blog.stevenlevithan.com/archives/javascript-roman-numeral-converter
 function romanize (num) {
     if (isNaN(num))
         return NaN;
-    var digits = String(+num).split(""),
-        key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
-            "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
-            "","I","II","III","IV","V","VI","VII","VIII","IX"],
+    let digits = String(+num).split(""),
+        key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",
+            "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
+            "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
         roman = "",
         i = 3;
     while (i--)
@@ -14,18 +14,18 @@ function romanize (num) {
 }
 
 function deromanize (str) {
-    var str = str.toUpperCase();
-    var validator = /^M*(?:D?C{0,3}|C[MD])(?:L?X{0,3}|X[CL])(?:V?I{0,3}|I[XV])$/;
-    var token = /[MDLV]|C[MD]?|X[CL]?|I[XV]?/g;
-    var key = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
-    var num = 0, m;
+    str = str.toUpperCase();
+    const validator = /^M*(?:D?C{0,3}|C[MD])(?:L?X{0,3}|X[CL])(?:V?I{0,3}|I[XV])$/;
+    const token = /[MDLV]|C[MD]?|X[CL]?|I[XV]?/g;
+    const key = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1};
+    let num = 0, m;
     if (!(str && validator.test(str))) return false;
     while (m = token.exec(str)) num += key[m[0]];
     return num;
 }
 
-var romanroman = document.getElementById("roman-roman");
-var romandecimal= document.getElementById("roman-decimal");
+let romanroman = document.getElementById("roman-roman");
+let romandecimal= document.getElementById("roman-decimal");
 
 romandecimal.onchange = function (){onRomanDecimalChange()};
 romanroman.onchange = function (){onRomanRomanChange()};
@@ -37,15 +37,14 @@ function onRomanDecimalChange(){
 }
 function onRomanRomanChange()
 {
-    let value = deromanize(romanroman.value);
-    romandecimal.value =value;
+    romandecimal.value =deromanize(romanroman.value);
 }
 
 //Binary
 
-var number = 0;
+let number = 0;
 let counter = document.getElementById("binaryCounter");
-var decCounter = document.getElementById("decimalBinaryCounter");
+let decCounter = document.getElementById("decimalBinaryCounter");
 let counterminus = document.getElementById("counter-minus");
 let counterplus= document.getElementById("counter-plus");
 
@@ -61,24 +60,25 @@ function binaryCounter(delta)
     }
     counterminus.disabled = number===0;
     counterplus.disabled = number===255;//I have not tested this.
-    var n = (number >>> 0).toString(2);
-    counter.innerText = "00000000".substr(n.length) + n;//todo deprecated?
+    let n = (number >>> 0).toString(2);
+    counter.innerText = "00000000".substring(n.length) + n;
     decCounter.innerText = number;
 }
 
 //Hex
 
-var hexhex = document.getElementById("hex-hex");
-var hexdec= document.getElementById("hex-decimal");
+let hexhex = document.getElementById("hex-hex");
+let hexdec= document.getElementById("hex-decimal");
 
 hexhex.onchange = function (){onHexHexChange()};
 hexdec.onchange = function (){onHexDecChange()};
 function onHexHexChange()
 {
     hexdec.value = parseInt(hexhex.value,16);
+    hexhex.value = hexhex.value.toUpperCase();
 }
 function onHexDecChange()
 {
-    var n = parseInt(hexdec.value);
-    hexhex.value = n.toString(16);
+    let n = parseInt(hexdec.value.toString());
+    hexhex.value = n.toString(16).toUpperCase();
 }
