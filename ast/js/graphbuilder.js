8a1b7f9bdbf7e19fecff23f8860b0a3b27ba98a1
hunterdyar <bloop@hdyar.com>
2025-02-22 18:30:58 -0500

    Refactor builder out to own script.

---
+++ ast/js/graphbuilder.js
@@ -0,0 +1,220 @@
+class GraphBuilder {
+    //running unique ID
+    wid = 0;
+    //ast nodes to graph node lookup for finding children
+    s = new Map();
+    //list of graph nodes. Together with the lookup, is all nodes.
+    g = []
+    //used for clusters.
+    childrenLookup = {}
+    nodeStack = [];
+    clusterStack = [];
+    lastPopped;
+    lastCluster;
+    constructor() {
+        this.wid = 0;
+    }
+
+    getID(){
+        this.wid++;
+        return this.wid;
+    }
+
+    AddRootNode(node){
+        //instantly clears the stack, adds new root node to entire system.
+    }
+    AddNode(node, name, decs = {}){
+        //adds a node and an edge to the last added node.
+        let me = new GNode(this.getID(),name);
+        //set decorators
+        Object.entries(decs).forEach((kv) => {
+            me.decs[kv[0]] = kv[1];
+        })
+        //are we in a cluster?
+        if(this.clusterPeek()){
+            this.clusterPeek().members.push(me.id);
+        }
+        //are we not a root node?
+        if(this.peek()){
+            this.g.push(new GEdge(this.getID(),this.peek().id,me.id,""))
+            //add self as child lookup to parent.
+            if(this.childrenLookup[me.id]){
+                this.childrenLookup[this.peek().id].push(me.id);
+            }else{
+                this.childrenLookup[this.peek().id]= [me.id];
+            }
+        }
+        this.nodeStack.push(me);
+        this.s.set(this.hash(node),me);
+    }
+    AddEdge(from,to,label="", decs={}){
+        let edge = new GEdge(this.getID(),this.s.get(this.hash(from)).id,this.s.get(this.hash(to)).id,label);
+        edge.decs = decs;
+        this.g.push(edge);
+
+    }
+    DoneWithNode(){
+        //pops node from stack, so children get added to all nodes.
+        this.lastPopped = this.nodeStack.pop();
+    }
+    StartCluster(name){
+        //marks a point for a cluster to be added.
+        let c = new GCluster(this.getID(),name);
+        this.clusterStack.push(c);
+        this.g.push(c);
+    }
+    EndCluster(){
+        //makes a point for a cluster to be removed.
+        this.lastCluster = this.clusterStack.pop();
+    }
+    AddDecorator(key,value){
+        //sets dec[key]=value on top node.
+    }
+    AddInvisibleNode(node){
+        if(this.lastPopped){
+            this.s.set(this.hash(node),this.lastPopped)
+        }
+    }
+    LabelIncomingEdge(node, label, decs={}){
+        let nodeID = this.s.get(this.hash(node)).id;
+        let edge = this.g.findLast(x=>{
+            return x.type === "edge" && x.toID === nodeID;
+        })
+        if(edge){
+            if(label) {
+                edge.label = label;
+            }
+            Object.entries(decs).forEach(x => {
+                edge.decs[x[0]] = x[1];
+            })
+        }else{
+            console.log("can't label incoming edge",node,label);
+        }
+    }
+    AddDecsToLastNode(decs){
+        if(this.lastPopped){
+            Object.entries(decs).forEach((kv) => {
+                this.lastPopped.decs[kv[0]] = kv[1];
+            })
+        }
+    }
+    peek(){
+        return this.nodeStack.at(-1);
+    }
+    clusterPeek(){
+        return this.clusterStack.at(-1);
+    }
+    ClusterID(){
+        if(this.clusterPeek()){
+            return this.clusterPeek().id;
+        }else{
+            console.log("no cluster for cluster ID")
+            return undefined;
+        }
+    }
+    GetGraphNodeFromNode(node){
+        return this.s.get(this.hash(node.left));
+    }
+
+    Build(){
+        let o = "digraph {\ncompound = true;\nsplines=ortho\n";
+        this.s.forEach(n => {
+            if(n.ignore){
+                return;
+            }
+            let d =""
+            for (const [key, value] of Object.entries(n.decs)) {
+                d+=`${key}=${value}`;
+            }
+            o += n.id+" [label=\""+n.label+"\" "+d+"]\n";
+        })
+        for(let i = 0; i < this.g.length; i++){
+            let n = this.g[i];
+            if(n.type === "edge"){
+                let d =""
+                for (const [key, value] of Object.entries(n.decs)) {
+                    d+=`${key}=${value}`;
+                }
+
+                o += n.fromID + " -> " + n.toID +" [label=\""+n.label+"\" "+d+"]";
+            }else if(n.type === "rank"){
+                o += "subgraph { rank = same; "+n.elements.join(" ")+" }";
+            }else if(n.type === "cluster"){
+                o+= "subgraph "+n.id+" { cluster = true;\n label="+n.label+";\n "+n.members.join("\n")+" }";
+            }
+            o+="\n"
+        }
+
+        o+=" }"
+        //console.log(o,state,g);
+        return o;
+    }
+
+    getAllChildren(root){
+        let c = {a:[]}
+        this.getChildrenRecursive(root,c);
+        return c.a;
+    }
+
+    getChildrenRecursive(root, current){
+        if(this.childrenLookup[root]){
+            this.childrenLookup[root].forEach((x)=>{
+                current.a.push(x);
+                this.getChildrenRecursive(x,current);
+            })
+        }
+    }
+
+    hash(node){
+        return node.start.toString()+":"+node.end.toString();
+    }
+}
+//linear or recursive? we're walking recursively elsewhere, so this can be linear.
+
+class GNode{
+    id
+    type
+    label
+    ignore = false
+    decs = {}
+    constructor(id, name){
+        this.label = name;
+        this.type = "node";
+        this.id = id;
+    }
+}
+class GEdge{
+    id
+    fromID
+    toID
+    label
+    type
+    decs = {}
+    constructor(id, f,t,label){
+        this.fromID = f;
+        this.toID = t;
+        this.label = label;
+        this.id = id;
+        this.type = "edge";
+    }
+}
+class GRank{
+    type = "rank";
+    elements;
+    constructor(...args) {
+        this.elements = args;
+    }
+}
+class GCluster {
+    id
+    type
+    label
+    members = []
+    decs = {}
+    constructor(id, name,...members){
+        this.label = name;
+        this.id = id;
+        this.type = "cluster";
+        this.members = members;
+    }
+}
--- ast/js/walker.js
+++ ast/js/walker.js
@@ -1,258 +1,100 @@
 
-let wid = 0;
-function getID(){
-    wid++;
-    return wid;
-}
-function hash(node){
-    return node.start.toString()+":"+node.end.toString();
-}
-//JS to GraphViz
+
+//JS to Builder. (Builder to graphbviz)
 const Walks = {
     ExpressionStatement(n,s,c){
         c(n.expression,s);
-        let nn = s.get(hash(n.expression));
-        //nn.decs["shape"] = "rectangle";
-        //we're skipping expressionStatement nodes, so it hashes to it's child.
-        s.set(hash(n),nn);
+        //we are our child.
+        s.AddInvisibleNode(n);
     },
     Literal(n,s,c){
-        let me = new GNode(n.value);
-        s.set(hash(n),me);
+        s.AddNode(n,n.value);
+        s.DoneWithNode();
     },
     Identifier(n,s,c){
-        let me = new GNode(n.name);
-        me.decs["shape"] = "note";
-        s.set(hash(n),me);
+        s.AddNode(n,n.name,{"shape":"note"})
+        s.DoneWithNode();
     },
     AssignmentExpression(n,s,c){
+        s.AddNode(n,"Assign");
         c(n.left, s);
-        let l = s.get(hash(n.left));
+        s.LabelIncomingEdge(n.left,"Left");
         c(n.right,s);
-        let r = s.get(hash(n.right));
-        let me = new GNode("Assign");
-        s.set(hash(n),me);
-        g.push(new GEdge(me.id,l.id,"left"))
-        //g.push(new GRank(l.id,l.id));
-        g.push(new GEdge(me.id,r.id,"right"))
-        //g.push(new GRank(l.id,r.id));
-        children[me.id] = [l.id, r.id];
-    }
-    ,
+        s.LabelIncomingEdge(n.right,"Right");
+        s.DoneWithNode();
+    },
     BinaryExpression(n,s,c) {
+        s.AddNode(n,n.operator);
         c(n.left, s);
-        let l = s.get(hash(n.left));
+        s.LabelIncomingEdge(n.left,"Left");
         c(n.right,s);
-        let r = s.get(hash(n.right));
-        let me = new GNode(n.operator);
-        s.set(hash(n),me);
-        g.push(new GEdge(me.id,l.id,"left"))
-        g.push(new GEdge(me.id,r.id,"right"))
-        children[me.id] = [l.id,r.id]
-       // g.push(new GRank(l.id,r.id));
+        s.LabelIncomingEdge(n.right,"Right");
+        s.DoneWithNode();
     },
     IfStatement(n,s,c){
+       s.StartCluster("if")
         c(n.test, s);
+        s.AddDecsToLastNode({"shape":"diamond"});
+        c(n.consequent,s);
 
-        let test = s.get(hash(n.test));
-        test.decs["shape"] = "diamond";
-        c(n.consequent, s);
-
-        let ifCluster = new GCluster("if", test.id);
-        ifCluster.ignore = true;
-        g.push(ifCluster);
-        let cons = s.get(hash(n.consequent));
+        s.AddEdge(n.test,n.consequent,"then",{"ltail":s.ClusterID()});//
         if(n.alternate){
             c(n.alternate, s);
-            let alt = s.get(hash(n.alternate));
-            let conEdge = new GEdge(test.id,alt.id,"else");
-            conEdge.decs["ltail"] = ifCluster.id;
-            g.push(conEdge)
+            s.AddEdge(n.test,n.alternate,"else",{"ltail":s.ClusterID()});//
         }
-        s.set(hash(n),ifCluster);
-        let thenEdge = new GEdge(test.id,cons.id,"then")
-        thenEdge.decs["ltail"] = ifCluster.id;
-        g.push(thenEdge)
-        let mems = getAllChildren(test.id);
-        ifCluster.members = ifCluster.members.concat(mems);
+        s.EndCluster();
     },
     BlockStatement(n,s,c){
-        let cluster = new GCluster("Block");
-        let me = new GNode("Block");
-        children[me.id] = [];
-        cluster.members.push(me.id);
-        cluster.ignore = false;
-        s.set(hash(n),me);
-
+        s.StartCluster("Block");
+        s.AddNode(n,"Block");
         var childCount = 0;
         n.body.forEach((x)=>{
             childCount++;//lol starting at 1
             c(x,s);
-            let xn = s.get(hash(x));
-            children[me.id].push(xn.id);
-            if(xn) {
-                cluster.members.push(xn.id);
-                let sequenceEdge = new GEdge(me.id,xn.id,childCount.toString());
-                g.push(sequenceEdge);
-            }
-            let blockChildren = getAllChildren(xn.id);
-            cluster.members = cluster.members.concat(blockChildren);
+            s.LabelIncomingEdge(x,childCount.toString());
         })
-        g.push(cluster)
+        s.DoneWithNode();
+        s.EndCluster();
     },
     ArrayExpression(n,s,c){
         console.log("ArrayExpression not supported yet sorry");
     },
     CallExpression(n,s,c){
-        //c(n.callee,s);
-        //var ident = s.get(hash(n.callee));
-        var me = new GNode(n.callee.name);
-        me.decs["shape"] = "pentagon";
-        s.set(hash(n),me);
+        s.AddNode(n.callee,n.callee.name,{"shape":"pentagon"});
+        s.AddInvisibleNode(n);
+
         if(arguments.length > 0) {
-            children[me.id] = [];
             let argCount = 0;//just used for labels
             n.arguments.forEach((x) => {
                 c(x, s);
-                let arg = s.get(hash(x));
-                var e = new GEdge(me.id, arg.id, "argument "+argCount);
-                g.push(e);
-                children[me.id].push(arg.id);
+                s.LabelIncomingEdge(x,"argument "+argCount.toString());
                 argCount++;
             })
         }
+        s.DoneWithNode();
     },
     UpdateExpression(n,s,c){
         //a++ or a--;
+        s.AddNode(n,n.operator);
         c(n.argument,s);
-        let arg = s.get(hash(n.argument));
-        var me = new GNode(n.operator);
-        s.set(hash(n),me);
-        var edge = new GEdge(me.id,arg.id,"");
-        g.push(edge);
-        children[me.id] = [arg.id];
+        s.DoneWithNode();
     },
     ConditionalExpression(n,s,c){
-        //ternery's have the same AST as conditioanls. basically.
         this.IfStatement(n,s,c);
     }
 }
 
-function getAllChildren(root){
-    let c = {a:[]}
-    getChildrenRecursive(root,c);
-    return c.a;
-}
-function getChildrenRecursive(root, current){
-    if(children[root]){
-        children[root].forEach((x)=>{
-            current.a.push(x);
-            getChildrenRecursive(x,current);
-        })
-    }
-}
-class GNode{
-    id
-    type
-    label
-    ignore = false
-    decs = {}
-    constructor(name){
-        this.label = name;
-        this.id = getID();
-        this.type = "node";
-    }
-}
-class GEdge{
-    id
-    fromID
-    toID
-    label
-    type
-    decs = {}
-    constructor(f,t,label){
-        this.fromID = f;
-        this.toID = t;
-        this.label = label;
-        this.id = getID();
-        this.type = "edge";
-    }
-}
-class GRank{
-    type = "rank";
-    elements;
-    constructor(...args) {
-        this.elements = args;
-    }
-}
-class GCluster {
-    id
-    type
-    label
-    members = []
-    decs = {}
-    constructor(name,...members){
-        this.label = name;
-        this.id = getID();
-        this.type = "cluster";
-        this.members = members;
-    }
-}
-
-let g =[]
-let children = {}
-
 function Convert(source){
-    g = []
-    children = []
-    wid = 0;
-    ast = {};
+    state = new GraphBuilder();
+
     try {
         ast = acorn.parse(source);
     }catch(err) {
         return null;
     }
     let o = "digraph {\ncompound = true;\nsplines=ortho\n"
-
-    let state = new Map();
     acorn.walk.recursive(acorn.parse(source),state,Walks);
 
-    state.forEach(n => {
-        if(n.ignore){
-            return;
-        }
-        let d =""
+    return state.Build();
 
-        for (const [key, value] of Object.entries(n.decs)) {
-            d+=`${key}=${value}`;
-        }
-            o += n.id+" [label=\""+n.label+"\" "+d+"]\n";
-    })
-    for(let i = 0; i < g.length; i++){
-        let n = g[i];
-         if(n.type === "edge"){
-             let d =""
-             for (const [key, value] of Object.entries(n.decs)) {
-                 d+=`${key}=${value}`;
-             }
-
-            o += n.fromID + " -> " + n.toID +" [label=\""+n.label+"\" "+d+"]";
-         }else if(n.type === "rank"){
-             o += "subgraph { rank = same; "+n.elements.join(" ")+" }";
-         }else if(n.type === "cluster"){
-             o+= "subgraph "+n.id+" { cluster = true;\n label="+n.label+";\n "+n.members.join("\n")+" }";
-         }
-        o+="\n"
-    }
-
-    o+=" }"
-    //console.log(o,state,g);
-    return o;
 }
-//digraph {
-//
-// 1[label="+",id=1]
-// 1 ->  2[label="left"]
-// 1 ->  3[label="right"]
-// subgraph{rank=same;2,3}
-// }
